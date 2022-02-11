5bab3cec761acda59639166cf49c13f3460c5b3d
Tuguldur <mtuguldur1097@gmail.com>
2022-02-11 15:50:01 +0800

    deploy scripts on node.js.yaml

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Layout, Menu, Button } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Salons from "./Salons";
+const { Header, Footer, Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Header>
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item key={1}>Hello</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ minHeight: 380 }}>
+          <Salons />
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    )
+  }
 }
 
 export default App;
---
+++ src/Salons.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Row, Col, Card } from "antd";
+class Salons extends Component{
+
+    render(){
+        return (
+            <Row justify="space-around" style={{padding:150}}>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+                <Col span={5}>
+                    <Card>
+                        <p>Hello </p>
+                    </Card>
+                </Col>
+            </Row>
+        );
+    }
+}
+
+export default Salons;
