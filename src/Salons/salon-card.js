cc91d7abe656f4c118ae10c590a4f635149c051e
tuguldurq <75665309+tuguldurq@users.noreply.github.com>
2022-02-26 13:01:50 +0800

    [feature] Salon card List for home page

---
+++ src/Salons/salon-card.js
@@ -0,0 +1,23 @@
+import { Row, Col } from "antd";
+import SalonCard from "../Components/Salon/Card";
+
+const style = {
+    padding: "50px 200px",
+    margin:0
+};
+
+function SalonList(props){
+    return (
+       <Row gutter={[48, 30]} justify="space-around" style={style}>
+           {props.salons.map(element => {
+                return (
+                    <Col sm={{span:6}} sm={6} xs={24}>
+                        <SalonCard></SalonCard>
+                    </Col>
+                )
+           })}
+       </Row>
+    );
+}
+
+export default SalonList;
