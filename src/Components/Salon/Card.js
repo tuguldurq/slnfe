d7da8e4688b9741061ddd7b40d13ff5e91d32e06
tuguldurq <75665309+tuguldurq@users.noreply.github.com>
2022-02-28 23:08:13 +0800

    [desing] salon card

--- src/Components/Salon/Card.js
+++ src/Components/Salon/Card.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import { Card } from "antd";
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card, Avatar } from "antd";
+
+const { Meta } = Card;
 
 class SalonCard extends Component{
     render(){
         return (
             <Card 
-                cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>} 
-                actions={[
-                    <SettingOutlined key="setting" />,
-                    <EditOutlined key="edit" />,
-                    <EllipsisOutlined key="ellipsis" />,
-                  ]}>
-                Card title
-
+                cover={<img alt="cover" className="cover" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                className="card"
+                hoverable="true">
+                <Meta
+                    avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                    title="Card title"
+                    description="This is the description"
+                    >
+                </Meta>
             </Card>
         )
     }
